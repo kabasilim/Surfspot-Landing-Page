4cc95e3d9731f097733eceb00f4f67d163c5c1f8
Abasilim Kosisochukwu Obaejeogu <obaejeoguabasilim@gmail.com>
2023-02-07 06:59:09 +0100

    Update and rename README.md to README.m

--- README.m
+++ README.m
@@ -2,14 +2,15 @@
 Surfspot is an IT firm that consults in computer hardware and internet network services. Its motto is your trusted IT solution provider.
 
 #VALUE SERVICES
-#Broadband wireless services
+
+#Broadband wireless services:
 Its value packages include
 4Mbps for 16k per month,
 6Mbps for 25k per month,
 7.5Mbps for 38k per month,
 9Mbps for 48.5k per month.
 
-#Infrastructure Consultancy
+#Infrastructure Consultancy:
 This covers
 Network design and Network operations center,
 Intercom systems,
@@ -20,8 +21,8 @@ Renewable Energy,
 
 #Superdealers in DSTV multichoice and SMILE
 
-#SURFSPOT TARGET AUDIENCE
+#SURFSPOT TARGET AUDIENCE -
 Surfspot does its practice in Edo state, Nigeria, covering Benin City especially, and venturing out to Uromi another part of Edo state.
 
-#SURFSPOT brand colors
+#SURFSPOT brand colors :
 Green and Blue
